672be5ec623e1b4e38f41850f1c9ad959ffff69e
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-04-30 10:40:36 +0200

    [Terminé] Job02

---
+++ jour02/job02/main.cpp
@@ -0,0 +1,12 @@
+// pour n = 0 le résultat est : 
+//     Nul
+//     Petit   (Il affiche également petit car pas de break)
+// pour n = 1 le résultat est : 
+//     Petit
+// pour n = 4 le résultat est :
+//     Moyen
+//     Grand (Il affiche également grand car pas de break)
+// pour n = 10 le résultat est : 
+//     Grand
+// pour n = -5 le résultat est : 
+//     Grand (car il prend en compte le default)
