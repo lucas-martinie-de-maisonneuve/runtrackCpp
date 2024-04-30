602a237aae16efb2bee75c53da51fc3c8db1165d
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-04-30 09:19:35 +0200

    [Terminé] Job04

---
+++ jour01/job04/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    float num1 = 10;
+    float num2 = 12;
+    float total = num1 + num2;
+    cout << "Le r"<<char(130)<<"sultat de " << num1 << " + " << num2 << " est " <<total;
+    return 0;
+}
