00eb5913e5506f9b7218810d7a7ed28d1e128052
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-05-04 16:07:12 +0200

    [Terminé] Jour04 job02

---
+++ jour04/job02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int a = 10;
+int b = 20;
+
+int main()
+{
+    int *ptrA = &a;
+    int *ptrB = &b;
+    int n = *ptrA;
+
+    cout << "Avant l'"<<char(130)<<"change :" << endl << "a = " << a << ", b = " << b << endl;
+
+    *ptrA = *ptrB;
+    *ptrB = n;
+
+    cout << "Apr\x8As l'"<<char(130)<<"change :" << endl << "a = " << a << ", b = " << b;
+    return 0;
+}
