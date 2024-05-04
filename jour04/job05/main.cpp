2fbc75fd063cd42e36d23b17bc05e656d4fd3477
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-05-04 17:35:27 +0200

    [Modifié] simplification de l'accès aux membres de la structure

--- jour04/job05/main.cpp
+++ jour04/job05/main.cpp
@@ -3,22 +3,19 @@ using namespace std;
 
 struct Point
 {
-    int x, y;
+    int x = 1, y = 2;
 };
 
 int main()
 {
     Point ePoint;
     Point *ptrPoint = &ePoint;
-    ePoint.x = 1;
-    ePoint.y = 2;
 
     cout << "Avant : x = " << ePoint.x << ", y = " << ePoint.y << endl;
 
-    ptrPoint->x = 111;
-    ptrPoint->y = 222;
+    ptrPoint->x = 111; ptrPoint->y = 222;
 
-    cout << "Apr\x82s : x = " << ePoint.x << ", y = " << ePoint.y << endl;
+    cout << "Apr\x8As : x = " << ePoint.x << ", y = " << ePoint.y << endl;
 
     return 0;
 }
