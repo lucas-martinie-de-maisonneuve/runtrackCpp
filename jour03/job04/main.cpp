6c5bf9964a39b97c443eca01a890d8bcfd4e09f2
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-05-04 10:35:34 +0200

    [Terminé] job04

---
+++ jour03/job04/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    const char *chaine1 = "Vive la plateforme !";
+    cout << "La taille est " <<  strlen(chaine1);
+    return 0;     
+}   
