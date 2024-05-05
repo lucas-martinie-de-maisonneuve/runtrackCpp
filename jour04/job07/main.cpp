b05cdfa0a1cd57c1e919c9d03106a78dfe8bc1ff
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-05-05 09:34:19 +0200

    [Modifié] type du tableau

--- jour04/job07/main.cpp
+++ jour04/job07/main.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-    char tableau[] = {9, 28, 12, 25, 12, 93};
+    int tableau[] = {9, 28, 12, 25, 12, 93};
 
     for (int i = 0; i < 6;  ++i) {
-        cout << "Adresse " << i << " : " << static_cast<void*>(&tableau[i]) << " - Valeur : " << static_cast<int>(tableau[i]) << endl;
+        cout << "Adresse " << i << " : " << (void *)&tableau[i] << " - Valeur : " << static_cast<int>(tableau[i]) << endl;
     }
 
     return 0;
