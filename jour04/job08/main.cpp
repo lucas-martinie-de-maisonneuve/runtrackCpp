565a0a0aa44e20acc4d6a0951e50f41b47f385b9
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-05-06 08:59:23 +0200

    [Modifié] Jour04 Job08

--- jour04/job08/main.cpp
+++ jour04/job08/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main()
-{
-    const char *tableau[4] = {"2019", "La Plateforme", "3.14", "Etudiants"};
-    for (int i = 0; i < 4; ++i)
-    {
-        cout << "Adresse " << i << " : " << (void *)&tableau[i] << ", Valeur : " << tableau[i] << endl;
-    }
-    return 0;
+int main(void) {
+
+    auto tab = make_tuple (2019, string ("La Plateforme"), 3.14, string("Etudiants")); 
+
+    cout << "Valeur 1 : " << get<0>(tab) << endl;
+    cout << "Valeur 2 : " << get<1>(tab) << endl;
+    cout << "Valeur 3 : " << get<2>(tab) << endl;
+    cout << "Valeur 4 : " << get<3>(tab) << endl;
+   
+    cout << "Adresse de Valeur 1 : " << &get<0>(tab) << endl;
+    cout << "Adresse de Valeur 2 : " << &get<1>(tab) << endl;
+    cout << "Adresse de Valeur 3 : " << &get<2>(tab) << endl;
+    cout << "Adresse de Valeur 4 : " << &get<3>(tab) << endl;
+
 }
